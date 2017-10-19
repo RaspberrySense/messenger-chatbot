acdca95ff5504d3906e3a4873800d9bc3d5acaa8
Mr <mrbotinhouse@gmail.com>
2017-10-19 18:20:42 +0530

    Add script for chatbot

---
+++ app.py
@@ -0,0 +1,59 @@
+import os, sys
+import random
+from flask import Flask, request
+from pymessenger import Bot
+
+app = Flask(__name__)
+
+PAGE_ACCESS_TOKEN = "***REMOVED***"
+
+bot = Bot(PAGE_ACCESS_TOKEN)
+
+greeting_list = ['hi','hey','hello','whats up']
+
+
+@app.route('/', methods=['GET'])
+def verify():
+	#Webhook verification
+	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+		if not request.args.get("hub.verify_token") == "hello":
+			return "Verification token mismatch", 403
+		return request.args["hub.challenge"], 200
+	return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+	data = request.get_json()
+	log(data)
+
+	if data['object'] == 'page':
+		for entry in data['entry']:
+			for messaging_event in entry['messaging']:
+
+				# IDs
+				sender_id = messaging_event['sender']['id']
+				recipient_id = messaging_event['recipient']['id']
+
+				if messaging_event.get('message'):
+					# Extracting text message
+					if 'text' in messaging_event['message']:
+						messaging_text = messaging_event['message']['text']
+					else:
+						messaging_text = 'nothing'
+
+					# Echo
+					messaging_text = random.choice(greeting_list)
+					response = messaging_text
+					bot.send_text_message(sender_id, response)
+
+	return "ok", 200
+
+
+def log(message):
+	print(message)
+	sys.stdout.flush()
+
+
+if __name__ == "__main__":
+	app.run(debug = True, port = 80)
