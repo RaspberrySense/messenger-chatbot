334d06f9fc991e606f4cbd9cd0f23129c079589b
Mr <mrbotinhouse@gmail.com>
2017-10-20 00:48:57 +0530

    dunno

---
+++ utils.py
@@ -0,0 +1,17 @@
+from wit import Wit
+
+access_token = "KG7AUFYAF6W5PTB7KBZ5CQVH32O24UIG"
+
+client = Wit(access_token = access_token)
+
+def wit_response(message_text):
+	resp = client.message(message_text)
+	entity = None
+	value = None
+
+	try:
+		entity = list(resp['entities'])[0]
+		value = resp['entities'][entity][0]['value']
+	except:
+		pass
+	return (entity, value)
