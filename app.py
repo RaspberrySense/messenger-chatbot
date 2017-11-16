246c367cf31c557e21cb4541bd4aaa768d316fd9
Mr <mrbotinhouse@gmail.com>
2017-11-16 21:59:43 +0530

    added random value of light and temp

--- app.py
+++ app.py
@@ -51,9 +51,9 @@ def webhook():
 
 				############ while script ###################
 
-					value1 = light_script()
-					response = value1
-					bot.send_text_message(sender_id, response)
+				value1 = light_script()
+				response = value1
+				bot.send_text_message(sender_id, response)
 
 				###############################################
 
