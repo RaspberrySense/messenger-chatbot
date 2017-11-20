d851dbfb2be932d692303128bb2c27eec0573bc3
Mr <mrbotinhouse@gmail.com>
2017-11-20 16:34:10 +0530

    greetings added

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ PAGE_ACCESS_TOKEN = "EAAa4SLDVfmYBAMWZAK6RkTRrE52AsWOI6lZCl50QVfOltUv7OuFvZBb9py
 
 bot = Bot(PAGE_ACCESS_TOKEN)
 
-greeting_list = ['hi','hey','hello','whats up','Hi','Hello','Hey']
+greeting_list = ['hi','hey','hello','Hi','Hello','Hey']
 thank_list = ['Thanks','Thank you','Thank you very much','thanks','thank you','thank you very much']
 thank_ret_list = ['No problem',"It's my job",'I am happy to help you', "It's my pleasure to serve you",'😇','☺']
 
@@ -50,13 +50,6 @@ def webhook():
 				sender_id = messaging_event['sender']['id']
 				recipient_id = messaging_event['recipient']['id']
 
-				############ while script ###################
-				'''value1 = light_script()
-				if value1 < 10 and value1 > 5:
-					bot.send_text_message(sender_id, value1)'''
-
-				##############################################
-
 				# FROM HERE>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 				if messaging_event.get('message'):
 					if (messaging_event['message'].get('nlp')):
@@ -70,52 +63,30 @@ def webhook():
 									if intent == "get_temp":							#T
 										temp_value = 'temp_script()'
 										response = 'temp_value'							#E
-									elif intent == "check_temp_low":
-										#if value == "cold":								#M
+									elif intent == "check_temp_low":								#M
 										response = 'check_temp_low()'
-										#else:											#P
-											#response = None
 									elif intent == "check_temp_high":					#E
-										#if value == "hot":
 										response = 'check_temp_high()'
-										#else:
-											#response = None
 									elif intent == "get_humidity":
 										response = 'humidity_script()'
 									elif intent == "check_humidity_low":
-										#if value == "dry":
 										response = 'check_humidity_low()'
-										#else:
-											#response = None
 									elif intent == "check_humidity_high":
-										#if value == "humid":
 										response = 'check_humidity_high()'
-										#else:
-											#response = None
 									elif intent == "check_light_on_or_off":
-										#if is_light_on():
 										response = "lights on"
-										#else:
-											#response "lights off"
 									elif intent == "check_light_off":
-										#if value == "dark":
 										response = 'check_light_off()'
-										#else:
-											#response = None
 									elif intent == "check_light_on":
-										#if value == "bright":
 										response = 'check_light_on()'
-										#else:
-											#response = None
 									elif intent == "get_image":
-										#if value == "image":
 										response = 'get_image()'
-										#elif value == "images":
-											#for img in wit/number:
-											#response = 'get_image()'
-												#bot.send_text_message(sender_id, response)
-												#time.sleep(60)
-											#flag_status = "Sent"
+									elif intent == "get_greeting":
+										response = random.choice(greeting_list)
+									elif intent == "get_thank":
+										response = random.choice(thank_ret_list)
+									elif intent == "return_thank":
+										response = random.choice(thank_list)
 									else:
 										response = "???"
 								bot.send_text_message(sender_id, response)
