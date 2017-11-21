891537f501b13c0bd6d6971a9f118074d6b5da85
Mr <mrbotinhouse@gmail.com>
2017-11-21 14:47:13 +0530

    sensors functions added

--- app.py
+++ app.py
@@ -46,67 +46,68 @@ def webhook():
                 sender_id = messaging_event['sender']['id']
                 SENDER_ID = sender_id
                 recipient_id = messaging_event['recipient']['id']
+                message = messaging_event.get('message')
+                if message:
+                    nlp_data = message.get('nlp')
+                    if nlp_data:
+                        intent_data = nlp_data['entities'].get('intent')
+                        if intent_data:
+                            intent = intent_data[0]['value']
+                                        #entity, value_list = messaging_event['message']['nlp']['entities'].items()
+                                        #value = value_list[0]['value']
 
-                nlp_data = messaging_event['message'].get('nlp')
-                if nlp_data:
-                    intent_data = nlp_data['entities'].get('intent')
-                    if intent_data:
-                        intent = intent_data[0]['value']
-                                    #entity, value_list = messaging_event['message']['nlp']['entities'].items()
-                                    #value = value_list[0]['value']
-
-                        if intent == "get_temp":                            
-                            response = get_temperature()                    
-                        elif intent == "check_temp_low":                                
-                            if is_cold():
-                                response = "Yes"
-                            elif is_warm():
-                                response = "Temperature is not low, it's normal"
-                        elif intent == "check_temp_high":                   
-                            if is_hot():
-                                response = "Yes"
-                            elif is_warm():
-                                response = "Temperature is not high, it's normal"
-                        elif intent == "get_humidity":
-                            response = get_humidity()
-                        elif intent == "check_humidity_low":
-                            if is_dry():
-                                response = "Yes"
-                            elif is_pleasant():
-                                response = "Humidity is not low, it's normal"
-                        elif intent == "check_humidity_high":
-                            if is_humid():
-                                response = "Yes"
-                            elif is_pleasant():
-                                response = "Humidity is not high, it's normal"
-                        elif intent == "check_light_on_or_off":
-                            if is_light_on():
-                                response = "Light is on"
+                            if intent == "get_temp":                            
+                                response = get_temperature()                    
+                            elif intent == "check_temp_low":                                
+                                if is_cold():
+                                    response = "Yes"
+                                elif is_warm():
+                                    response = "Temperature is not low, it's normal"
+                            elif intent == "check_temp_high":                   
+                                if is_hot():
+                                    response = "Yes"
+                                elif is_warm():
+                                    response = "Temperature is not high, it's normal"
+                            elif intent == "get_humidity":
+                                response = get_humidity()
+                            elif intent == "check_humidity_low":
+                                if is_dry():
+                                    response = "Yes"
+                                elif is_pleasant():
+                                    response = "Humidity is not low, it's normal"
+                            elif intent == "check_humidity_high":
+                                if is_humid():
+                                    response = "Yes"
+                                elif is_pleasant():
+                                    response = "Humidity is not high, it's normal"
+                            elif intent == "check_light_on_or_off":
+                                if is_light_on():
+                                    response = "Light is on"
+                                else:
+                                    response = "Light is off"
+                            elif intent == "check_light_off":
+                                if is_light_on() == False:
+                                    response = "Yes"
+                                else:
+                                    response = "No"
+                            elif intent == "check_light_on":
+                                if is_light_on():
+                                    response = "Yes"
+                                else:
+                                    response = "No"
+                            elif intent == "get_image":
+                                response = capture_image()
+                            elif intent == "get_greeting":
+                                response = random.choice(greeting_list)
+                            elif intent == "get_thank":
+                                response = random.choice(thank_ret_list)
+                            elif intent == "return_thank":
+                                response = random.choice(thank_list)
+                            elif intent == "get_talk":
+                                response = random.choice(talk_list)
                             else:
-                                response = "Light is off"
-                        elif intent == "check_light_off":
-                            if is_light_on() == False:
-                                response = "Yes"
-                            else:
-                                response = "No"
-                        elif intent == "check_light_on":
-                            if is_light_on():
-                                response = "Yes"
-                            else:
-                                response = "No"
-                        elif intent == "get_image":
-                            response = capture_image()
-                        elif intent == "get_greeting":
-                            response = random.choice(greeting_list)
-                        elif intent == "get_thank":
-                            response = random.choice(thank_ret_list)
-                        elif intent == "return_thank":
-                            response = random.choice(thank_list)
-                        elif intent == "get_talk":
-                            response = random.choice(talk_list)
-                        else:
-                            response = "???"
-                        bot.send_text_message(sender_id, response)
+                                response = "???"
+                            bot.send_text_message(sender_id, response)
 
 
 
