0e4d6d455df83e91fe2de2994bb7b907d598bada
Faheel Ahmad <faheel@live.in>
2017-11-25 02:40:56 +0530

    Update camera script

--- sensors/camera.py
+++ sensors/camera.py
@@ -1,6 +1,24 @@
+from datetime import datetime
+
+from picamera import PiCamera
+
+
+camera = PiCamera()
+
+
 def capture_image():
-    return "Captured image"
+    dt = datetime.now().strftime('%Y-%m-%d.%H:%M:%S')
+    image_path = '/tmp/image.{}.jpg'.format(dt)
+    camera.capture(image_path)
+
+    return image_path
 
 
 def capture_video(duration=10):
-    return "Captured video for {} seconds".format(duration)
+    dt = datetime.now().strftime('%Y-%m-%d.%H:%M:%S')
+    video_path = '/tmp/video.{}.h264'.format(dt)
+    camera.start_recording(video_path)
+    camera.wait_recording(duration)
+    camera.stop_recording()
+
+    return video_path
