bd45999399e18db8f1bba8a9032e50b2aa9a0458
Faheel Ahmad <faheel@live.in>
2017-11-24 16:32:04 +0530

    Refactor and make use of generated replies

--- app.py
+++ app.py
@@ -1,24 +1,37 @@
 import os
-import sys
 import random
+import sys
+import time
+
 from flask import Flask, request
-from utils import wit_response
 from pymessenger import Bot
+
+from sensors import camera, humidity, light, temperature, motion
+from replies import get_sensor_reply
 import secrets
 
+
+# Globals ---------------------------------------------------------------------
 app = Flask(__name__)
-
 bot = Bot(secrets.PAGE_ACCESS_TOKEN)
-
-greeting_list = ['hi', 'hey', 'hello', "what's up", 'Hi', 'Hello', 'Hey']
-thank_list = ['Thanks', 'Thank you', 'Thank you very much',
-              'thanks', 'thank you', 'thank you very much']
-thank_ret_list = ['No problem', "It's my job",
-                  'I am happy to help you', "It's my pleasure to serve you",
-                  '😇', '☺']
+sender_id = None
 
 
-# Webhook verification
+# Helpers ---------------------------------------------------------------------
+def get_bot():
+    return bot
+
+
+def get_sender_id():
+    return sender_id
+
+
+def log(message):
+    print(message)
+    sys.stdout.flush()
+
+
+# Webhook verification --------------------------------------------------------
 @app.route('/', methods=['GET'])
 def verify():
     mode = request.args.get('hub.mode')
@@ -34,67 +47,168 @@ def verify():
     return 'Nothing to do', 200
 
 
+# -----------------------------------------------------------------------------
 @app.route('/', methods=['POST'])
 def webhook():
+    global sender_id
+
     data = request.get_json()
     log(data)
 
     if data['object'] == 'page':
         for entry in data['entry']:
             for messaging_event in entry['messaging']:
-                # IDs
                 sender_id = messaging_event['sender']['id']
                 recipient_id = messaging_event['recipient']['id']
 
-                nlp_data = messaging_event['message'].get('nlp')
-                if nlp_data:
-                    intent_data = nlp_data['entities'].get('intent')
-                    if intent_data:
-                        intent = intent_data[0]['value']
-                        # entity, value_list = nlp_data['entities'].items()
-                        # value = value_list[0]['value']
-
-                        if intent == 'get_temp':
-                            temp_value = 'temp_script()'
-                            response = 'temp_value'
-                        elif intent == 'check_temp_low':
-                            response = 'check_temp_low()'
-                        elif intent == 'check_temp_high':
-                            response = 'check_temp_high()'
-                        elif intent == 'get_humidity':
-                            response = 'humidity_script()'
-                        elif intent == 'check_humidity_low':
-                            response = 'check_humidity_low()'
-                        elif intent == 'check_humidity_high':
-                            response = 'check_humidity_high()'
-                        elif intent == 'check_light_on_or_off':
-                            response = 'lights on'
-                        elif intent == 'check_light_off':
-                            response = 'check_light_off()'
-                        elif intent == 'check_light_on':
-                            response = 'check_light_on()'
-                        elif intent == 'get_image':
-                            response = 'get_image()'
-                        elif intent == 'get_greeting':
-                            response = random.choice(greeting_list)
-                        elif intent == 'get_thank':
-                            response = random.choice(
-                                thank_ret_list)
-                        elif intent == 'return_thank':
-                            response = random.choice(thank_list)
-                        elif intent == 'get_talk':
-                            response = random.choice(talk_list)
-                        else:
-                            response = "Sorry! I didn't understand"
-                            bot.send_text_message(sender_id, response)
+                message_data = messaging_event.get('message')
+                if message_data:
+                    nlp_data = message_data.get('nlp')
+                    if nlp_data:
+                        intent_data = nlp_data['entities'].get('intent')
+                        number_data = nlp_data['entities'].get('number')
+                        if intent_data:
+                            intent = intent_data[0]['value']
+                            value = None
+                            if number_data:
+                                value = number_data[0]['value']
+                            send_response(sender_id, intent, value)
 
     return 'OK', 200
 
 
-def log(message):
-    print(message)
-    sys.stdout.flush()
+# Send response ---------------------------------------------------------------
+def send_response(sender_id, intent, value):
+    # Temperature:
+    if intent == 'get_temp':
+        temp = temperature.get_temperature()
+        response = get_sensor_reply('temperature', intent, value=temp)
+
+    elif intent == 'check_temp_low':
+        if temperature.is_cold():
+            result = 'yes'
+        elif temperature.is_hot():
+            result = 'no'
+        else:
+            result = 'other'
+        response = get_sensor_reply('temperature', intent, result)
+
+    elif intent == 'check_temp_high':
+        if temperature.is_hot():
+            result = 'yes'
+        elif temperature.is_cold():
+            result = 'no'
+        else:
+            result = 'other'
+        response = get_sensor_reply('temperature', intent, result)
+
+    elif intent == 'check_temp_value_below':
+        if value:
+            if temperature.get_temperature() < value:
+                result = 'yes'
+            else:
+                result = 'no'
+            response = get_sensor_reply('temperature', intent, result, value)
+        else:
+            response = 'Can you phrase that more clearly, please'
+
+    elif intent == 'check_temp_value_above':
+        if value:
+            if temperature.get_temperature() > value:
+                result = 'yes'
+            else:
+                result = 'no'
+            response = get_sensor_reply('temperature', intent, result, value)
+        else:
+            response = 'Can you phrase that more clearly, please'
+
+    # Humidity:
+    elif intent == 'get_humidity':
+        humidity = humidity.get_humidity()
+        response = get_sensor_reply('humidity', intent, value=humidity)
+
+    elif intent == 'check_humidity_low':
+        if humidity.is_dry():
+            result = 'yes'
+        elif humidity.is_humid():
+            result = 'no'
+        else:
+            result = 'other'
+        response = get_sensor_reply('humidity', intent, result)
+
+    elif intent == 'check_humidity_high':
+        if humidity.is_humid():
+            result = 'yes'
+        elif humidity.is_dry():
+            result = 'no'
+        else:
+            result = 'other'
+        response = get_sensor_reply('humidity', intent, result)
+
+    elif intent == 'check_humidity_value_below':
+        if value:
+            if humidity.get_humidity() < value:
+                result = 'yes'
+            else:
+                result = 'no'
+            response = get_sensor_reply('humidity', intent, result, value)
+        else:
+            response = 'Can you phrase that more clearly, please'
+
+    elif intent == 'check_humidity_value_above':
+        if value:
+            if humidity.get_humidity() > value:
+                result = 'yes'
+            else:
+                result = 'no'
+            response = get_sensor_reply('humidity', intent, result, value)
+        else:
+            response = 'Can you phrase that more clearly, please'
+
+    # TODO: Light:
+    elif intent == 'check_light_on_or_off':
+        if light.is_light_on():
+            response = 'Light is on'
+        else:
+            response = 'Light is off'
+
+    elif intent == 'check_light_off':
+        if not light.is_light_on():
+            response = 'Yes'
+        else:
+            response = 'No'
+
+    elif intent == 'check_light_on':
+        if light.is_light_on():
+            response = 'Yes'
+        else:
+            response = 'No'
+
+    # Camera:
+    elif intent == 'get_image':
+        response = capture_image()
+
+    # Motion:
+
+    # Other:
+    elif intent == 'get_greeting':
+        response = random.choice(greeting_list)
+
+    elif intent == 'get_thank':
+        response = random.choice(thank_ret_list)
+
+    elif intent == 'return_thank':
+        response = random.choice(thank_list)
+
+    elif intent == 'get_talk':
+        response = random.choice(talk_list)
+
+    else:
+        response = "Sorry, I don't understand"
+
+    bot.send_text_message(sender_id, response)
 
 
+# -----------------------------------------------------------------------------
 if __name__ == '__main__':
     app.run(debug=True, port=80)
