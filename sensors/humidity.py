df865b4d877c48f1e53b34990f99104fa3622f9e
Faheel Ahmad <faheel@live.in>
2017-11-25 11:46:50 +0530

    Initialise sensors to give correct reading on the first try

--- sensors/humidity.py
+++ sensors/humidity.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 import pigpio
-import DHT22
+from sensors import DHT22
 
 
 pi = pigpio.pi()
@@ -10,13 +10,19 @@ dht22 = DHT22.sensor(pi, 27)
 HUMID_LOWER_LIMIT = 75  # percent
 DRY_UPPER_LIMIT = 25    # percent
 
+is_initialised = False
+
 
 def init_sensor():
     dht22.trigger()
     sleep(0.5)
+    global is_initialised
+    is_initialised = True
 
 
 def get_humidity():
+    if not is_initialised:
+        init_sensor()
     dht22.trigger()
 
     return dht22.humidity()
