fbacbca02cdd80795128af206a124b4c61c0509b
Faheel Ahmad <faheel@live.in>
2017-11-25 11:45:02 +0530

    Update file name template

--- sensors/camera.py
+++ sensors/camera.py
@@ -7,16 +7,16 @@ camera = PiCamera()
 
 
 def capture_image():
-    dt = datetime.now().strftime('%Y-%m-%d.%H:%M:%S')
-    image_path = '/tmp/image.{}.jpg'.format(dt)
+    dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    image_path = '/tmp/image {}.jpg'.format(dt)
     camera.capture(image_path)
 
     return image_path
 
 
 def capture_video(duration=10):
-    dt = datetime.now().strftime('%Y-%m-%d.%H:%M:%S')
-    video_path = '/tmp/video.{}.h264'.format(dt)
+    dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    video_path = '/tmp/video {}.h264'.format(dt)
     camera.start_recording(video_path)
     camera.wait_recording(duration)
     camera.stop_recording()
