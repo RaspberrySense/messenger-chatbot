b16e58b6ebfca6c294122eb8cb871d3ae7e73510
Faheel Ahmad <faheel@live.in>
2017-11-25 11:49:05 +0530

    Fix errors

--- sensors/light.py
+++ sensors/light.py
@@ -2,8 +2,8 @@ from gpiozero import LightSensor
 
 
 LIGHT_THRESHOLD = 0.9		# min = 0, max = 1
-ldr = LightSensor(4, LIGHT_THRESHOLD)
+ldr = LightSensor(4, threshold=LIGHT_THRESHOLD)
 
 
 def is_light_on():
-    return ldr.light_detected()
+    return ldr.light_detected
