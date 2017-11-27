8cdd1c5a2e5248e7cb8fd2e12ba4a64b578606fe
Faheel Ahmad <faheel@live.in>
2017-11-28 03:05:39 +0530

    Update camera script

--- sensors/camera.py
+++ sensors/camera.py
@@ -3,30 +3,27 @@ from datetime import datetime
 from picamera import PiCamera
 
 
-camera = None
-
-
-def init_camera():
-    global camera
-    if not camera:
-        camera = PiCamera()
-
-
 def capture_image():
-    init_camera()
-    dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    image_path = '/tmp/image {}.jpeg'.format(dt)
-    camera.capture(image_path)
-
-    return image_path
+    try:
+        with PiCamera() as camera:
+            camera.rotation = 180
+            dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            image_path = '/tmp/image {}.jpeg'.format(dt)
+            camera.capture(image_path)
+        return image_path
+    except:
+        return None
 
 
 def capture_video(duration=10):
-    init_camera()
-    dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    video_path = '/tmp/video {}.h264'.format(dt)
-    camera.start_recording(video_path)
-    camera.wait_recording(duration)
-    camera.stop_recording()
-
-    return video_path
+    try:
+        with PiCamera() as camera:
+            camera.rotation = 180
+            dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            video_path = '/tmp/video {}.h264'.format(dt)
+            camera.start_recording(video_path)
+            camera.wait_recording(duration)
+            camera.stop_recording()
+        return video_path
+    except:
+        return None
