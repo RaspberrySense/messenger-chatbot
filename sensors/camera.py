bf4413a1e2b7d83882f628e9efe8f2eb39947746
Faheel Ahmad <faheel@live.in>
2017-11-27 05:55:14 +0530

    Add initialisation function

--- sensors/camera.py
+++ sensors/camera.py
@@ -3,18 +3,26 @@ from datetime import datetime
 from picamera import PiCamera
 
 
-camera = PiCamera()
+camera = None
+
+
+def init_camera():
+    global camera
+    if not camera:
+        camera = PiCamera()
 
 
 def capture_image():
+    init_camera()
     dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    image_path = '/tmp/image {}.jpg'.format(dt)
+    image_path = '/tmp/image {}.jpeg'.format(dt)
     camera.capture(image_path)
 
     return image_path
 
 
 def capture_video(duration=10):
+    init_camera()
     dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     video_path = '/tmp/video {}.h264'.format(dt)
     camera.start_recording(video_path)
--- sensors/light.py
+++ sensors/light.py
@@ -1,9 +1,16 @@
 from gpiozero import LightSensor
 
 
-LIGHT_THRESHOLD = 0.9		# min = 0, max = 1
-ldr = LightSensor(4, threshold=LIGHT_THRESHOLD)
+LIGHT_THRESHOLD = 0.9       # min = 0, max = 1
+ldr = None
+
+
+def init_sensor():
+    global ldr
+    if not ldr:
+        ldr = LightSensor(4, threshold=LIGHT_THRESHOLD)
 
 
 def is_light_on():
+    init_sensor()
     return ldr.light_detected
